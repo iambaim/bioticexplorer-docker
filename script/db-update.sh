13511a978f157880275b0d835d0a9efa6579cd62
Ibrahim Umar <ibrahim.umar@hi.no>
2021-04-18 22:11:48 +0200

    Ensure db-update.sh won't fail when called from cron

--- script/db-update.sh
+++ script/db-update.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # Do the update
-docker-compose exec dbserver sh -c "rm -fR /var/monetdb5/dbfarm/bioticexplorer-next && monetdb create bioticexplorer-next && monetdb release bioticexplorer-next"
-docker-compose exec bioticexplorer sh -c "TMPDIR=/data Rscript --vanilla  -e \"BioticExplorerServer::compileDatabase(dbIndexPath = Sys.getenv('IDX_PATH'))\""
+docker-compose exec -T dbserver sh -c "rm -fR /var/monetdb5/dbfarm/bioticexplorer-next && monetdb create bioticexplorer-next && monetdb release bioticexplorer-next"
+docker-compose exec -T bioticexplorer sh -c "TMPDIR=/data Rscript --vanilla  -e \"BioticExplorerServer::compileDatabase(dbIndexPath = Sys.getenv('IDX_PATH'))\""
 
 # Only do bulk update command below when bioticexplorer-next is available
-docker-compose exec dbserver sh -c "monetdb stop bioticexplorer-next" && \
-docker-compose exec dbserver sh -c "monetdb stop bioticexplorer" && \
-docker-compose exec dbserver sh -c "rm -fR /var/monetdb5/dbfarm/bioticexplorer && monetdb set name=bioticexplorer bioticexplorer-next && monetdb start bioticexplorer" && \
+docker-compose exec -T dbserver sh -c "monetdb stop bioticexplorer-next" && \
+docker-compose exec -T dbserver sh -c "monetdb stop bioticexplorer" && \
+docker-compose exec -T dbserver sh -c "rm -fR /var/monetdb5/dbfarm/bioticexplorer && monetdb set name=bioticexplorer bioticexplorer-next && monetdb start bioticexplorer" && \
 docker-compose restart bioticexplorer
 
