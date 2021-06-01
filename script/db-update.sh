5e3bf4a7f6b7c8870bfdb81c752a98af0d953f0f
Ibrahim Umar <ibrahim.umar@hi.no>
2021-06-01 20:05:17 +0200

    Update db-update.sh to ignore missing bioticexplorer database (or first run)

--- script/db-update.sh
+++ script/db-update.sh
@@ -2,11 +2,11 @@
 
 # Do the update
 docker-compose exec -T dbserver sh -c "rm -fR /var/monetdb5/dbfarm/bioticexplorer-next && monetdb create bioticexplorer-next && monetdb release bioticexplorer-next"
-docker-compose exec -T bioticexplorer sh -c "TMPDIR=/data Rscript --vanilla  -e \"BioticExplorerServer::compileDatabase(dbIndexPath = Sys.getenv('IDX_PATH'))\""
+docker-compose exec -T bioticexplorer sh -c "TMPDIR=/data Rscript --vanilla  -e \"library(BioticExplorerServer);compileDatabase(dbIndexPath = Sys.getenv('IDX_PATH'))\""
 
 # Only do bulk update command below when bioticexplorer-next is available
 docker-compose exec -T dbserver sh -c "monetdb stop bioticexplorer-next" && \
-docker-compose exec -T dbserver sh -c "monetdb stop bioticexplorer" && \
-docker-compose exec -T dbserver sh -c "rm -fR /var/monetdb5/dbfarm/bioticexplorer && monetdb set name=bioticexplorer bioticexplorer-next && monetdb start bioticexplorer" && \
+docker-compose exec -T dbserver sh -c "monetdb stop bioticexplorer && rm -fR /var/monetdb5/dbfarm/bioticexplorer"
+docker-compose exec -T dbserver sh -c "monetdb set name=bioticexplorer bioticexplorer-next && monetdb start bioticexplorer" && \
 docker-compose restart bioticexplorer
 
